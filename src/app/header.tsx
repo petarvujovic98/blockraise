d33774c17292015fe4c834d15bbe5672139a9776
Petar Vujovic <petarvujovic98@gmail.com>
2023-12-06 11:42:45 +0100

    feat: Add logo and name in header

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Navbar } from "./navbar";
 import { UserMenu } from "./user-menu";
 
@@ -5,6 +6,16 @@ export function Header() {
   return (
     <header className="flex w-full flex-col items-stretch justify-start gap-4 bg-vibrant-blue py-4">
       <div className="mx-auto flex w-full max-w-screen-xl flex-row items-center justify-between px-4">
+        <div className="flex flex-row items-center justify-start gap-2">
+          <Image
+            src="/blockraise.png"
+            unoptimized
+            width={60}
+            height={60}
+            className="rounded-full"
+          />
+          <b className="text-5xl font-bold text-deep-navy-blue">BlockRaise</b>
+        </div>
         <Navbar />
         <UserMenu />
       </div>
